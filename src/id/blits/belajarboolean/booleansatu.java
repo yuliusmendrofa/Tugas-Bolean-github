c18f882c53a69cc5962bc7bfcaab446c5ca2b36a
yuliusmendrofa <ymendrofa.ym@gmail.com>
2014-10-24 18:14:11 -1200

    tugas boolean

---
+++ src/id/blits/belajarboolean/booleansatu.java
@@ -0,0 +1,15 @@
+package id.blits.belajarboolean;
+
+
+public class booleansatu {
+
+    
+    public static void main(String[] args) {
+    
+        boolean hasil;
+        
+        hasil = 2<5;
+        
+        System.out.println(hasil);
+    }
+}
